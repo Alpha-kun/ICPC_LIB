380256b80ac0a38b852f63d735ed35caa5ea4a7d
Charlie <CharlieLiuAlpha@gmail.com>
2021-12-27 20:26:55 -0500

    added clion error code

--- debug/debug.cpp
+++ debug/debug.cpp
@@ -6,3 +6,8 @@ void show(vector<int> &v) {
     for (int i = 0; i < v.size(); ++i) cout << setw(4) << v[i];
     cout << endl;
 }
+
+//0xC0000374 heap corruption
+//0xC0000005 invalid memory access
+//0xC00000FD stackoverflow
+//0xC0000094 divide by zero
