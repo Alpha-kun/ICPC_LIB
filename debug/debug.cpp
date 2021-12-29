955c46dcfd68a370d143fe2a14d64e625cf8d58e
Charlie <CharlieLiuAlpha@gmail.com>
2021-12-29 06:05:46 -0500

    added assert

--- debug/debug.cpp
+++ debug/debug.cpp
@@ -1,5 +1,5 @@
+#include "assert.h"
 #define watch(x) cout << (#x) << " is " << (x) << endl
-
 void show(vector<int> &v) {
     for (int i = 0; i < v.size(); ++i) cout << setw(4) << i;
     cout << endl;
